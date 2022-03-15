ac835211dbb78ec0f3896709e3af1c5f702996cf
Souljacker1 <sameerpyo39@gmail.com>
2022-03-15 16:32:23 +0530

    Bringup for Arcana Official

--- aosp_RMX2170.mk
+++ aosp_RMX2170.mk
@@ -7,13 +7,28 @@
 # Inherit from RMX2170 device
 $(call inherit-product, device/realme/RMX2170/device.mk)
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# First, inherit Project Arcana common.mk
+$(call inherit-product, vendor/aosp/common.mk)
+
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-EXTENDED_BUILD_TYPE := OFFICIAL
-
-# Inherit some common AospExtended stuff
-$(call inherit-product, vendor/aosp/common.mk)
+# Other Props
+ARCANA_OFFICIAL := true
+ARCANA_MAINTAINER := SAMMY
+WITH_GAPPS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_BLUR := true
+COPY_APN_SYSTEM := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_SUPPORTS_NOW_PLAYING := true
+USE_PIXEL_CHARGER_IMAGES := true
 
 # Device identifier
 PRODUCT_NAME := aosp_RMX2170
